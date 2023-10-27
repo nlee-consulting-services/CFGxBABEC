f880ab8fa178b719b90ca9ad68b57355505d6002
Abdalla <a.eltayeb@campus.tu-berlin.de>
2023-10-26 19:48:52 -0700

    second draft entry form

--- my-app/src/components/pages/EntryFormPage.js
+++ my-app/src/components/pages/EntryFormPage.js
@@ -10,30 +10,76 @@ function EntryForm(){
             <div class = "entryform-wrapper">
                 <h1>Entry Form</h1>
                 <form action="/action_page.php">
-                    <label for="fname">First name:</label><br/>
-                    <input type="text" id="fname" name="fname"/><br/>
-                    <label for="lname">Last name:</label><br/>
-                    <input type="text" id="lname" name="lname"/><br/>
-                    <label for="lname">Teacher Last name:</label><br/>
-                    <input type="text" id="tlname" name="tlname"/><br/>
-                    <label for="schoolname">School name:</label> <br/>
-                    <select name="schoolname" id="schoolname"> 
-                        <option >school1</option> 
-                        <option>school2</option> 
-                        <option >school3</option> 
-                        <option>school4</option> 
-                    </select><br/>
-                    <label for="insectname">Insect name:</label> <br/>
-                    <select name="insectname" id="insectname"> 
-                        <option >bug1</option> 
-                        <option>bug2</option> 
-                        <option >bug3</option> 
-                        <option>bug4</option> 
-                    </select><br/>
-                    <label for="Longitude">Longitude:</label><br/>
-                    <input type="text" id="Longitude" name="Longitude"/><br/>
-                    <label for="Latitude">Latitude:</label><br/>
-                    <input type="text" id="Latitude" name="Latitude"/><br/>
+
+                    <div class = "student-data">
+                        <h2>Student Data</h2>
+
+                        <label for="initials">Initials(e.g. John Doe → JD):</label><br/>
+                        <input type="text" id="initials" name="initials"/><br/>
+                        <label for="lname">Teacher Last name:</label><br/>
+                        <input type="text" id="tlname" name="tlname"/><br/>
+                        <label for="orgname">Organization name:</label> <br/>
+
+                        <select name="orgname" id="orgname"> 
+                            <option >school1</option> 
+                            <option>school2</option> 
+                            <option >school3</option> 
+                            <option>school4</option> 
+                        </select><br/>
+                    </div>
+                    <br/>
+                    <br/>
+
+                    <div class = "record">
+                        <h2>Insect information</h2>
+
+                        <label for="ordername">Order name:</label> <br/>
+                        <select name="ordername" id="ordername"> 
+                            <option >bug1</option> 
+                            <option>bug2</option> 
+                            <option >bug3</option> 
+                            <option>bug4</option> 
+                        </select><br/>
+
+                        <label for="commonname">Common name:</label><br/>
+                        <input type="text" id="commonname" name="commonname"/><br/>
+                        <label for="insect-description">Insect description:</label><br/>
+                        <input type="text" id="insect-description" name="insect-description"/><br/>
+                        <label for="date-of-collection">Date of Collection:</label><br/>
+                        <input type="date" id="date-of-collection" name="date-of-collection"/><br/>
+                        <label for="habitat-description">Habitat description:</label><br/>
+                        <input type="text" id="habitat-description" name="habitat-description"/><br/>
+
+                        <label for="presence-of-wolbachia">Presence of Wolbachia:</label> <br/>
+                        <select name="presence-of-wolbachia" id="presence-of-wolbachia"> 
+                            <option >Yes</option> 
+                            <option>No</option> 
+                            <option >Inconclusive</option> 
+                        </select><br/>
+
+                        <label for="confidence-levek">Confidence level:</label> <br/>
+                        <select name="presence-of-wolbachia" id="presence-of-wolbachia"> 
+                            <option >High</option> 
+                            <option>Low</option> 
+                        </select><br/>
+
+                        <label for="explanantion-of-confidence-level">Explanantion of confidence level:</label><br/>
+                        <input type="text" id="explanantion-of-confidence-level" name="explanantion-of-confidence-level"/><br/>
+                        <label for="gel-image">Gel image (Please use JPG or PNG format):</label><br/>
+                        <input type="file" id="gel-image" name="gel-image" accept="image/png, image/jpeg"/><br/>
+                    </div>
+                    <br/>
+                    <br/>
+
+                    <div class = "location">
+                        <h2>Location</h2>
+
+                        <label for="Longitude">Longitude:</label><br/>
+                        <input type="text" id="Longitude" name="Longitude"/><br/>
+                        <label for="Latitude">Latitude:</label><br/>
+                        <input type="text" id="Latitude" name="Latitude"/><br/>
+                    </div>
+
                     <br/><input type="submit" value="Submit"/>
                 </form>
             </div> 
