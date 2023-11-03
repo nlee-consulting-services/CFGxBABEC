1c2e415086766183c8ecf6524d763b86f261c2a3
Hyojeong Yoon <emilyyoon3107@gmail.com>
2023-11-02 18:46:52 -0700

    feat: GET orgs api

--- backend/src/controllers/orgs.controller.js
+++ backend/src/controllers/orgs.controller.js
@@ -1,8 +1,10 @@
+import { getOrgs } from "../services/orgs.js";
+
 async function get(req, res, next) {
   try {
     console.log("routed to orgs");
-    // const rows = await getRecords(req.body);
-    // res.json(rows);
+    const rows = await getOrgs();
+    res.json(rows);
   } catch (err) {
     console.error(`Error while getting orgs`, err.message);
     next(err);
