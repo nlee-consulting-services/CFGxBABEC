d47dc665485384c31161d3874a72f24659a815f5
emi-sakamoto <ejsakamoto3@gmail.com>
2023-11-02 19:49:14 -0700

    homepage changes

--- my-app/src/components/pages/HomePage.js
+++ my-app/src/components/pages/HomePage.js
@@ -9,9 +9,17 @@ function HomePage(){
         <div class='wrapper'>
             <Navbar />
             <div className="contentwrapper">
-                <img className = "hero" src= "./heroimg.png"/>
+                <div className ="hero">header section
+                </div>
                 <div className="map">
-                    <img src = "./map.jpeg"/>
+                    <div className = "aboutoverlay" >
+                        <h1>About the Project</h1>
+                        <p>this is a website used to track wolbachia here is some filler text. This text is talking about what the wolbachia tracking site does and why it is omportant. More filler text to make this sectio longer talking about what wolbachia is how it is so important things liek that.  </p>
+                    </div>
+                </div>
+                <div className= "engagement">
+                    <h1>Get involved</h1>
+                    <p> here is how you can get involved in wolnachia tracking. here is some text explaining how to get involved there should be a form to the right. You can look at that and see what sort of work you need to do to get involved. We need your help. </p>
                 </div>
             </div>
             <Footer />
