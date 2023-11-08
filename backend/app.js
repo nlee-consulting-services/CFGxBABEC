4ac0b2df7fbd748bb319365770fc9cfb1d04cb56
Hyojeong Yoon <emilyyoon3107@gmail.com>
2023-11-07 16:15:40 -0800

    fix: allow all CORS origins

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ import { teacherRouter } from "./src/routes/teachers.route.js";
 
 const app = express();
 app.use(express.json());
+app.use(cors({ origin: "*" }));
 
 const hostname = process.env.hostname || "127.0.0.1";
 const port = process.env.port || 3000;
