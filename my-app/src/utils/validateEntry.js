280b966fcef59bd9964c928901f1766d0eca26c7
Hyojeong Yoon <emilyyoon3107@gmail.com>
2023-11-09 19:24:29 -0800

    feat: validate longitude and latitude

--- my-app/src/utils/validateEntry.js
+++ my-app/src/utils/validateEntry.js
@@ -1,14 +1,19 @@
 import { studentEndpoint, teacherEndpoint } from "./endpoints";
 
+// stautus -1: error in entry --> user needs to fix entry
+// status -2: teacher does not exist in org --> prompt if wants to add teacher
+// status -3: student does not exist under teacher --> prompt if wants to add student
+// status 1: no errors, ready to add to database
 const checkValidity = async (
   entry,
   name_initial,
   teacher_last_name,
-  org_id
+  org_id,
+  org_name
 ) => {
   // check existence of org_id
   if (!org_id) {
-    return false;
+    return { status: -1, errmsg: "Please select a valid organization" };
   }
 
   // check if the teacher exists in the given organization
@@ -24,7 +29,10 @@ const checkValidity = async (
     }
   }
   if (!teacher_id) {
-    return false;
+    return {
+      status: -2,
+      errmsg: `Teacher with last name ${teacher_last_name} does not exist under institution ${org_name}.`,
+    };
   }
 
   // check if the given teacher exists under the given teacher
@@ -40,7 +48,19 @@ const checkValidity = async (
     }
   }
   if (!student_id) {
-    return false;
+    return {
+      status: -3,
+      errmsg: `Student with initials ${name_initial} does not exist under instructor ${teacher_last_name} at ${org_name}.`,
+    };
+  }
+
+  // check if longtitude is within range
+  if (entry.location_lon < -180 || entry.location_lon > 180) {
+    return { status: -1, errmsg: "Please provide a valid longitude" };
+  }
+  // check if latitude is within range
+  if (entry.location_lat < -90 || entry.location_lat > 90) {
+    return { status: -1, errmsg: "Please provide a valid latitude" };
   }
 };
 const addTeacher = () => {};
