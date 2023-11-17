6bfd907afe6d94a3a43f94a5eed78cb15e0c4adf
Jun Hyung Lee <meatjun@berkeley.edu>
2023-11-16 19:29:54 -0800

    finished navbar responsiveness

--- my-app/src/components/navbar.js
+++ my-app/src/components/navbar.js
@@ -1,29 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
-import"./navbar.css";
+import "./navbar.css";
 
-const Navbar = () => {    
+const Navbar = () => {
+    const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+
+    const toggleDropdown = () => {
+        setIsDropdownVisible(!isDropdownVisible);
+    };
+
     return (
         <nav className='navbar'>
-            <img className = "logo" src="./logo.png"/>
+            <img className="logo" src="./logo.png" alt="Logo"/>
             <h1>Wolbachia Map</h1>
-            <ul className='list'>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                <li>
-                    <Link to="/map">Map</Link>
-                </li>
-                <li>
-                    <Link to="/entryform">Entry Form</Link>
-                </li>
+            <button className="dropdown-toggle" onClick={toggleDropdown}>
+                ☰ {/* Hamburger Icon */}
+            </button>
+            <ul className={`list ${isDropdownVisible ? 'show' : ''}`}>
+                <li><Link to="/" onClick={toggleDropdown}>Home</Link></li>
+                <li><Link to="/about" onClick={toggleDropdown}>About</Link></li>
+                <li><Link to="/map" onClick={toggleDropdown}>Map</Link></li>
+                <li><Link to="/entryform" onClick={toggleDropdown}>Entry Form</Link></li>
             </ul>
         </nav>
     );
 };
-
 
 export default Navbar;
