b0b8116cffb191aa99561939fddf1c2f1e8b9ea6
Hyojeong Yoon <emilyyoon3107@gmail.com>
2023-11-16 18:39:01 -0800

    fix: make css activated for modal

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import logo from "./logo.svg";
 //import React from 'react';
-import './App.css';
-import { BrowserRouter , Route, Routes } from 'react-router-dom'; 
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
 
-import Home from './components/pages/HomePage'
-import About from './components/pages/AboutPage'
-import MapPage from './components/pages/MapPage'
-import EntryForm from './components/pages/EntryFormPage'
-import navbar from './components/navbar'
+import Home from "./components/pages/HomePage";
+import About from "./components/pages/AboutPage";
+import MapPage from "./components/pages/MapPage";
+import EntryForm from "./components/pages/EntryFormPage";
+import navbar from "./components/navbar";
 
 function App() {
   return (
-    <div className='wrapper'>
+    <div className="wrapper">
       <BrowserRouter>
         <Routes>
-          <Route path='/' exact Component={Home} />
-          <Route path='/about' Component={About} />
-          <Route path='/map' Component={MapPage} />
-          <Route path='/entryform' Component={EntryForm} />
-        </Routes> 
+          <Route path="/" exact Component={Home} />
+          <Route path="/about" Component={About} />
+          <Route path="/map" Component={MapPage} />
+          <Route path="/entryform" Component={EntryForm} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
