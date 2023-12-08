ccbd802374d07f69169fa70de16d2489ba949dd6
Hyojeong Yoon <emilyyoon3107@gmail.com>
2023-12-08 02:40:26 -0800

    style: footer restyling with grid and flexbox

--- my-app/src/components/footer.js
+++ my-app/src/components/footer.js
@@ -1,29 +1,39 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./footer.css";
 
 const Footer = () => {
   return (
     <footer>
       <div className="footer-wrapper">
-        <img className="babec-logo" src="./babec_logo_bw.png" />
-        <img className="about1" />
-        <a className="aboutlink1" href="https://babec.org/what-we-do/">
-          About BABEC
-        </a>
-        <img className="cfgbanner" src="./CFG Banner.png" />
-        <img className="about2" />
-        <a className="aboutlink2" href="https://codeforgoodberkeley.github.io/">
-          About CFG
-        </a>
-        <img className="email1" />
-        <a className="emaillink1" href="mailto:babec@babec.org">
-          babec@babec.org
-        </a>
-        <img className="email2" />
-        <a className="emaillink2" href="mailto:codeforgoodberkeley@gmail.com">
-          codeforgoodberkeley@gmail.com
-        </a>
+        <div className="footer-logo-holder">
+          <img className="babec-logo" src="./babec_logo_bw.png" />
+        </div>
+        <div className="footer-link-grid">
+          <img className="about1" src="./info_icon.svg" />
+          <a className="aboutlink1" href="https://babec.org/what-we-do/">
+            About BABEC
+          </a>
+          <img className="email1" src="./email_icon.svg" />
+          <a className="emaillink1" href="mailto:babec@babec.org">
+            babec@babec.org
+          </a>
+        </div>
+        <div className="footer-logo-holder">
+          <img className="cfg-logo" src="./CFG Banner.png" />
+        </div>
+        <div className="footer-link-grid">
+          <img className="about2" src="./info_icon.svg" />
+          <a
+            className="aboutlink2"
+            href="https://codeforgoodberkeley.github.io/"
+          >
+            About CFG
+          </a>
+          <img className="email2" src="./email_icon.svg" />
+          <a className="emaillink2" href="mailto:codeforgoodberkeley@gmail.com">
+            codeforgoodberkeley@gmail.com
+          </a>
+        </div>
       </div>
     </footer>
   );
