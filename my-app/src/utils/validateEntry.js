99b7b8a095b77ca90e3582250a6e7fb99603f0f3
Hyojeong Yoon <emilyyoon3107@gmail.com>
2023-12-07 17:33:44 -0800

    feat: structure validation flow by parts

--- my-app/src/utils/validateEntry.js
+++ my-app/src/utils/validateEntry.js
@@ -12,21 +12,60 @@ const checkValidity = async (
   org_id,
   org_name
 ) => {
-  // check existence of org_id
-  if (!org_id) {
-    return { status: -1, errmsg: "Please select a valid organization" };
-  }
-
-  //check existence of teacher
-  if (!teacher_last_name) {
-    return { status: -1, errmsg: "Please enter your teacher's last name" };
-  }
-
+  // ** Part 1: Check all mandatory fields **//
   //check existence of student initial
   if (!name_initial) {
     return { status: -1, errmsg: "Please enter your initials" };
   }
+  //check existence of teacher
+  if (!teacher_last_name) {
+    return { status: -1, errmsg: "Please enter your teacher's last name" };
+  }
+  // check existence of org_id
+  if (!org_id) {
+    return { status: -1, errmsg: "Please select a valid organization" };
+  }
+  // check existence of order_id
+  if (!entry.order_id) {
+    return { status: -1, errmsg: "Please select your sample's insect order" };
+  }
+  //check existence of common name
+  if (!entry.common_name) {
+    return {
+      status: -1,
+      errmsg: "Please enter a common name for your insect sample",
+    };
+  }
+  //check existence of collection date
+  if (!entry.collection_date) {
+    return { status: -1, errmsg: "Please enter the collection date" };
+  }
+  //check existence of wolbachia presence
+  if (!entry.wolbachia_presence) {
+    return { status: -1, errmsg: "Please indicate wolbachia presence" };
+  }
+  // check existence of confidence level
+  if (!entry.confidence_level) {
+    return { status: -1, errmsg: "Please indicate confidence level" };
+  }
 
+  //** Part 2: Validate values without DB communication **//
+  // check if longtitude is within range
+  if (entry.location_lon < -180 || entry.location_lon > 180) {
+    return {
+      status: -1,
+      errmsg: "Please provide a valid longitude (range: -180<x<180)",
+    };
+  }
+  // check if latitude is within range
+  if (entry.location_lat < -90 || entry.location_lat > 90) {
+    return {
+      status: -1,
+      errmsg: "Please provide a valid latitude (range: -90<x<90)",
+    };
+  }
+
+  //** Part 3: Validate values with DB communication **//
   // check if the teacher exists in the given organization
   const teacherList = await axios({
     method: "get",
@@ -45,7 +84,6 @@ const checkValidity = async (
       errmsg: `Teacher with last name "${teacher_last_name}" does not exist under institution "${org_name}". If you are certain this is not a mistake, press the "Add Teacher" button and resubmit.`,
     };
   }
-
   // check if the given teacher exists under the given teacher
   const studentList = await axios({
     method: "get",
@@ -62,21 +100,6 @@ const checkValidity = async (
     return {
       status: -3,
       errmsg: `Student with initials "${name_initial}" does not exist under instructor "${teacher_last_name}" at "${org_name}." If you are certain this is not a mistake, press the "Add Student" button and resubmit`,
-    };
-  }
-
-  // check if longtitude is within range
-  if (entry.location_lon < -180 || entry.location_lon > 180) {
-    return {
-      status: -1,
-      errmsg: "Please provide a valid longitude (range: -180<x<180)",
-    };
-  }
-  // check if latitude is within range
-  if (entry.location_lat < -90 || entry.location_lat > 90) {
-    return {
-      status: -1,
-      errmsg: "Please provide a valid latitude (range: -90<x<90)",
     };
   }
 };
