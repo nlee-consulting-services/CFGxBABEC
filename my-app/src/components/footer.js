b2d732f764829db3e61ed90a1619bfee0683912a
Hyojeong Yoon <emilyyoon3107@gmail.com>
2023-12-08 01:37:27 -0800

    style: footer restyling attempt with grid

--- my-app/src/components/footer.js
+++ my-app/src/components/footer.js
@@ -5,32 +5,25 @@ import "./footer.css";
 const Footer = () => {
   return (
     <footer>
-      <div class="row">
-        <ul>
-          {/*please fix the design it  s ugly :( -alex*/}
-          <img className="footerlogo" src="./babec_logo_bw.png" />
-          <div className="divider"></div>
-          <img className="cfgbanner" src="./CFG Banner.png" />
-          <div className="link">
-            <li>
-              <strong>Links</strong>
-            </li>
-            <li>
-              <a href="https://babec.org/what-we-do/">About BABEC</a>
-            </li>
-            <li>
-              <a href="mailto:babec@babec.org">babec@babec.org</a>
-            </li>
-            <li>
-              <a href="https://codeforgoodberkeley.github.io/">About CFG</a>
-            </li>
-            <li>
-              <a href="mailto:codeforgoodberkeley@gmail.com">
-                codeforgoodberkeley@gmail.com
-              </a>
-            </li>
-          </div>
-        </ul>
+      <div className="footer-wrapper">
+        <img className="babec-logo" src="./babec_logo_bw.png" />
+        <img className="about1" />
+        <a className="aboutlink1" href="https://babec.org/what-we-do/">
+          About BABEC
+        </a>
+        <img className="cfgbanner" src="./CFG Banner.png" />
+        <img className="about2" />
+        <a className="aboutlink2" href="https://codeforgoodberkeley.github.io/">
+          About CFG
+        </a>
+        <img className="email1" />
+        <a className="emaillink1" href="mailto:babec@babec.org">
+          babec@babec.org
+        </a>
+        <img className="email2" />
+        <a className="emaillink2" href="mailto:codeforgoodberkeley@gmail.com">
+          codeforgoodberkeley@gmail.com
+        </a>
       </div>
     </footer>
   );
