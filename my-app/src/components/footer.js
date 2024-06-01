dd8af8c5816fea6e7cf3dfc7679d3f94953d8689
Lee & Associates Consulting Services <47929994+nlee-consulting-services@users.noreply.github.com>
2024-06-01 11:36:50 -0700

    Update footer.js

--- my-app/src/components/footer.js
+++ my-app/src/components/footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
         </div>
         <div className="footer-link-grid">
           <img className="about1" src="./info_icon.svg" />
-          <a className="aboutlink1" href="https://babec.org/what-we-do/">
+          <a className="aboutlink1" href="https://babec.org/what-we-do/" target={"_blank"}>
             About BABEC
           </a>
           <img className="email1" src="./email_icon.svg" />
@@ -26,6 +26,7 @@ const Footer = () => {
           <a
             className="aboutlink2"
             href="https://codeforgoodberkeley.github.io/"
+            target={"_blank"}
           >
             About CFG
           </a>
